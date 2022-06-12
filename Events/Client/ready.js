380dae75472835fa52159d3c8022dd39963ec607
StroosDev <102289594+StroosDev@users.noreply.github.com>
2022-06-12 23:13:23 +0100

    Update ready.js

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,10 +1,9 @@
 const { Client } = require('discord.js');
-require('colors');
 
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        console.log(`${client.user.tag} (${client.user.id}) is ready and up to go!`.orange);
+        console.log(`${client.user.tag} (${client.user.id}) is ready and up to go!`);
     }
 }
